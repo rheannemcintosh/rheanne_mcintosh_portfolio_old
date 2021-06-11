a37c5f4ea1ea7ebdd7ae476aaef3897c6bdc1b93
rheannemcintosh <rheannemcintosh@gmail.com>
2021-06-11 23:46:49 +0100

    Add the table and the mass assingable fields

--- app/Models/Experience.php
+++ app/Models/Experience.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Experience extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     * 
+     * @var string
+     */
+    protected $table = 'experiences_employers';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'employer_name',
+        'role',
+        'employment_type',
+        'start_date',
+        'end_date',
+        'location',
+        'country',
+        'example',
+    ];
 }
