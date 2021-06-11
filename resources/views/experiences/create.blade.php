ba5cd753a9930c4aa4ebdfc0c423fde9b3cc5476
rheannemcintosh <rheannemcintosh@gmail.com>
2021-06-11 23:45:25 +0100

    Fix names for the experience create form

--- resources/views/experiences/create.blade.php
+++ resources/views/experiences/create.blade.php
@@ -1,26 +1,26 @@
 <form action="{{ route('experiences.store') }}" method="post">@csrf
     <div>
-        <label for="employer">Employer</label>
-        <input name="employer"type="text">
+        <label for="employer_name">Employer</label>
+        <input name="employer_name"type="text">
     </div>
     <div>
         <label for="role">Role</label>
         <input name="role" type="text">
     </div>
     <div>
-        <label for="type">Type</label>
-        <select name="type">
+        <label for="employment_type">Type</label>
+        <select name="employment_type">
             <option value="Full Time">Full Time</option>
             <option value="Part Time">Part Time</option>
         </select>
     </div>
     <div>
-        <label for="end-date">Start Date</label>
-        <input for="end-date" type="date">
+        <label for="start_date">Start Date</label>
+        <input for="start_date" type="date">
     </div>
     <div>
-        <label for="end-date">End Date</label>
-        <input for="end-date" type="date">
+        <label for="end_date">End Date</label>
+        <input for="end_date" type="date">
     </div>
     <div>
         <label for="location">Location</label>
@@ -28,7 +28,7 @@
     </div>
     <div>
         <label for="country">Country</label>
-        <select name="county">
+        <select name="country">
             <option value="United Kingdom">United Kingdom</option>
         </select>
     </div>
