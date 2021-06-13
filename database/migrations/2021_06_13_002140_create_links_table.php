034273a5616dd17adc1c19301d85ec1a02125b91
rheannemcintosh <rheannemcintosh@gmail.com>
2021-06-13 01:54:56 +0100

    Create the links table

---
+++ database/migrations/2021_06_13_002140_create_links_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->id();
+            $table->text('name');
+            $table->string('url', 2083);
+            $table->foreignId('link_type_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+}
