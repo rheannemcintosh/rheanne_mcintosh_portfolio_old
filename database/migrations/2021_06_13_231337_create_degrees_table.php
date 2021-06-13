66ba75268e7ae0d2c1c5b75802b87a07b0b8a719
rheannemcintosh <rheannemcintosh@gmail.com>
2021-06-14 00:18:56 +0100

    Create the migration for the degrees table

---
+++ database/migrations/2021_06_13_231337_create_degrees_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDegreesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('degrees', function (Blueprint $table) {
+            $table->id();
+            $table->string('university_name');
+            $table->string('degree_name');
+            $table->string('degree_type');
+            $table->string('degree_classification');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('degrees');
+    }
+}
