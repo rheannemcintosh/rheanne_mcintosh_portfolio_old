acf29a3b90a79f6cb22f308377a10d92f3f73fbc
rheannemcintosh <rheannemcintosh@gmail.com>
2021-06-18 00:43:42 +0100

    Create the Experience Detail Model

---
+++ app/Models/ExperienceDetail.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ExperienceDetail extends Model
+{
+    use HasFactory;
+
+     /**
+     * The table associated with the model.
+     * 
+     * @var string
+     */
+    protected $table = 'experience_details';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'detail',
+    ];
+}
