8c1f2cef56af3ece5a1c99e3fc3056dfd2b11f44
rheannemcintosh <rheannemcintosh@gmail.com>
2021-06-20 23:34:01 +0100

    Create the Link Controller

---
+++ app/Http/Controllers/LinkController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Link;
+
+class LinkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $links = Link::all();
+
+        return view('links.index', compact('links'));
+    }
+}
