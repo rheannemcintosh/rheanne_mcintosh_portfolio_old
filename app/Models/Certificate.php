4b0d58902758d5b2f2f191dca83ab1e9a79163ee
rheannemcintosh <rheannemcintosh@gmail.com>
2021-06-24 23:51:52 +0100

    Update the table and fillable fields for the Certificate model

--- app/Models/Certificate.php
+++ app/Models/Certificate.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Certificate extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     * 
+     * @var string
+     */
+    protected $table = 'certificates';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'date',
+    ];
 }
