61e5ece579726c8aacb9f91a47835269c4ab91c4
rheannemcintosh <rheannemcintosh@gmail.com>
2021-06-25 23:39:09 +0100

    Return all certificates to the template

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Certificate;
 
 class CertificateController extends Controller
 {
@@ -13,7 +14,9 @@ class CertificateController extends Controller
      */
     public function index()
     {
-        //
+        $certificates = Certificate::all();
+
+        return view('certificates.index', compact('certificates'));
     }
 
     /**
