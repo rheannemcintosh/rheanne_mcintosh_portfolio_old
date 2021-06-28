6f153d38dc6b8beb122d2c68f70ca180c91f5a39
rheannemcintosh <rheannemcintosh@gmail.com>
2021-06-28 21:06:54 +0100

    Add the Degree details to the CV template

--- app/Http/Controllers/CVController.php
+++ app/Http/Controllers/CVController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Degree;
 
 class CVController extends Controller
 {
@@ -13,6 +14,8 @@ class CVController extends Controller
      */
     public function index()
     {
-        return view('cv.index');
+        $degrees = Degree::all();
+
+        return view('cv.index', compact('degrees'));
     }
 }
