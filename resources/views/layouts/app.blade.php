1b4aa9927e33a6e16d4516f579bbf4f936195006
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2021-07-15 23:40:00 +0100

    Update the app layout file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,46 +1,26 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Head -->
+    @include('includes.head')
 
-        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
-        <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
-
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-        <!-- Scripts -->
-        <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-        <script src="{{ asset('js/app.js') }}" defer></script>
-    </head>
+    <!-- Body -->
     <body class="font-sans antialiased">
-    @include('includes.nav')
-        <div class="page-wrap min-h-screen">
 
-            <!-- Page Heading -->
-            <header class="shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                </div>
-            </header>
+        <!-- Navigation -->
+        @include('includes.nav')
+
+        <div class="page-wrap min-h-screen">
 
             <!-- Page Content -->
             <main>
                 {{ $slot }}
             </main>
+
         </div>
+
+        <!-- Footer -->
         @include('includes.footer')
+
     </body>
 </html>
