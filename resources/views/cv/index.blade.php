5971bee41cb6c4ddef61a7a88915de0d7962e260
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2021-07-29 23:59:08 +0100

    Update the CV page with some holder text to improve formatting

--- resources/views/cv/index.blade.php
+++ resources/views/cv/index.blade.php
@@ -1,95 +1,118 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('CV') }}
-        </h2>
-    </x-slot>
+    <div class="container mx-auto p-4 bg-red-500">
+        <div class="p-4 lg:px-24 bg-green-700">
+            <div class="bg-rmgrey shadow rounded-md p-8">
+                <div class="bg-yellow-400">
+                <h1>Hi, I'm Rheanne</h1>
+                <p>
+                    Summary About Rheanne: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                </div>
+                <div class="mt-4 bg-yellow-400">
+                    <h1 class="text-3xl">Experience</h1>
+                    <div class="mt-4 bg-blue-50">
+                        <h1>Visualsoft Ltd.</h1>
+                    </div>
+                    <div class="mt-4 bg-blue-50">
+                        <h1>University of Aberdeen</h1>
+                    </div>
+                </div>
+                <div class="mt-4 bg-yellow-400">
+                    <h1 class="text-3xl">Education</h1>
+                    <div class="mt-4 bg-blue-50">
+                        <h1>BSc (Hons) Computing (Graphics and Animation) | 1st Class Honours</h1>
+                        <h2>Robert Gordon University</h2>
+                        <h3>2014 - 2018</h3>
+                    </div>
+                    <div class="mt-4 bg-blue-50">
+                        <h1>Bridge of Don Academy</h1>
+                        <h3>2008 - 2014</h3>
+                    </div>
+                </div>
 
-    <div class="py-12">  
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                
-                <!-- This example requires Tailwind CSS v2.0+ -->
-                <div class="bg-white shadow overflow-hidden sm:rounded-lg">
-                    <div class="px-4 py-5 sm:px-6">
-                        <div class="px-4 py-5 sm:px-6">
-                            <h3 class="text-lg leading-6 font-medium text-gray-900">
-                                Rheanne McIntosh
-                            </h3>
-                        </div>
-                        <div class="border-t border-gray-200">
-                            <p>----- Insert Summary -----</p>
+
+            <div class="py-12">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+
+                        <!-- This example requires Tailwind CSS v2.0+ -->
+                        <div class="bg-white shadow overflow-hidden sm:rounded-lg">
+                            <div class="px-4 py-5 sm:px-6">
+                                <div class="px-4 py-5 sm:px-6">
+                                    <h3 class="text-lg leading-6 font-medium text-gray-900">
+                                        Rheanne McIntosh
+                                    </h3>
+                                </div>
+                                <div class="border-t border-gray-200">
+                                    <p>----- Insert Summary -----</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
 
-    <div class="py-12">  
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <!-- This example requires Tailwind CSS v2.0+ -->
-                <div class="bg-white shadow overflow-hidden sm:rounded-lg">
-                    <div class="px-4 py-5 sm:px-6">
-                        <div class="px-4 py-5 sm:px-6">
-                            <h3 class="text-lg leading-6 font-medium text-gray-900">
-                                Experience
-                            </h3>
-                        </div>
-                        <div class="border-t border-gray-200">
-                            <p>----- Insert Experience -----</p>
+            <div class="py-12">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <!-- This example requires Tailwind CSS v2.0+ -->
+                        <div class="bg-white shadow overflow-hidden sm:rounded-lg">
+                            <div class="px-4 py-5 sm:px-6">
+                                <div class="px-4 py-5 sm:px-6">
+                                    <h3 class="text-lg leading-6 font-medium text-gray-900">
+                                        Experience
+                                    </h3>
+                                </div>
+                                <div class="border-t border-gray-200">
+                                    <p>----- Insert Experience -----</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
 
-    <div class="py-12">  
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <!-- This example requires Tailwind CSS v2.0+ -->
-                <div class="bg-white shadow overflow-hidden sm:rounded-lg">
-                    <div class="px-4 py-5 sm:px-6">
-                        <div class="px-4 py-5 sm:px-6">
-                            <h3 class="text-lg leading-6 font-medium text-gray-900">
-                                Education
-                            </h3>
-                        </div>
-                        <div class="border-t border-gray-200">
-                            @foreach ($degrees as $degree)
-                                <h4>{{ $degree->university_name }} | {{ $degree->degree_type }} {{$degree->degree_name }}</h4>
-                                <h5>{{ $degree->degree_classification }}</h5>
-                                <h6>{{ $degree->start_date->format('Y') }} - {{ $degree->end_date->format('Y') }}</h6>
-                            @endforeach
-                            <p>----- Insert Education -----</p>
+            <div class="py-12">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <!-- This example requires Tailwind CSS v2.0+ -->
+                        <div class="bg-white shadow overflow-hidden sm:rounded-lg">
+                            <div class="px-4 py-5 sm:px-6">
+                                <div class="px-4 py-5 sm:px-6">
+                                    <h3 class="text-lg leading-6 font-medium text-gray-900">
+                                        Education
+                                    </h3>
+                                </div>
+                                <div class="border-t border-gray-200">
+
+                                    <p>----- Insert Education -----</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
 
-    <div class="py-12">  
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <!-- This example requires Tailwind CSS v2.0+ -->
-                <div class="bg-white shadow overflow-hidden sm:rounded-lg">
-                    <div class="px-4 py-5 sm:px-6">
-                        <div class="px-4 py-5 sm:px-6">
-                            <h3 class="text-lg leading-6 font-medium text-gray-900">
-                                Certificates
-                            </h3>
-                        </div>
-                        <div class="border-t border-gray-200">
-                            <p>----- Insert Certificates -----</p>
+            <div class="py-12">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <!-- This example requires Tailwind CSS v2.0+ -->
+                        <div class="bg-white shadow overflow-hidden sm:rounded-lg">
+                            <div class="px-4 py-5 sm:px-6">
+                                <div class="px-4 py-5 sm:px-6">
+                                    <h3 class="text-lg leading-6 font-medium text-gray-900">
+                                        Certificates
+                                    </h3>
+                                </div>
+                                <div class="border-t border-gray-200">
+                                    <p>----- Insert Certificates -----</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+            </div>
         </div>
     </div>
-
-
 </x-app-layout>
