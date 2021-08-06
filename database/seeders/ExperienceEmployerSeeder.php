2032378511abb7edd19ed7bbbc03e0e6dd5541dd
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2021-08-06 23:20:48 +0100

    Create the Experience Employer Seeder

---
+++ database/seeders/ExperienceEmployerSeeder.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ExperienceEmployer;
+use Illuminate\Database\Seeder;
+
+class ExperienceEmployerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      \DB::table('experience_employers')->delete();
+
+      ExperienceEmployer::create([
+        'employer_name' => 'Visualsoft',
+        'start_date' => '2021-06-01 00:00:00',
+        'end_date' => null,
+        'location' => 'Remote (Stockton-on-Tees)',
+        'country' => 'UK',
+        'example' => false,
+      ]);
+
+      ExperienceEmployer::create([
+        'employer_name' => 'University of Aberdeen',
+        'start_date' => '2019-07-01 00:00:00',
+        'end_date' => '2021-05-27 00:00:00',
+        'location' => 'Polwarth, Aberdeen',
+        'country' => 'UK',
+        'example' => false,
+      ]);
+
+      ExperienceEmployer::create([
+        'employer_name' => 'Sainsburys',
+        'start_date' => '2014-09-01 00:00:00',
+        'end_date' => '2014-09-01 00:00:00',
+        'location' => 'Garthdee, Aberdeen',
+        'country' => 'UK',
+        'example' => false,
+      ]);
+
+      ExperienceEmployer::create([
+        'employer_name' => 'Stompers Nursery',
+        'start_date' => '2014-09-01 00:00:00',
+        'end_date' => '2014-09-01 00:00:00',
+        'location' => 'Bridge of Don, Aberdeen',
+        'country' => 'UK',
+        'example' => false,
+      ]);
+
+      ExperienceEmployer::create([
+        'employer_name' => 'Techno-Play',
+        'start_date' => '2014-09-01 00:00:00',
+        'end_date' => '2014-09-01 00:00:00',
+        'location' => 'Bridge of Don, Aberdeen',
+        'country' => 'UK',
+        'example' => false,
+      ]);
+
+      ExperienceEmployer::create([
+        'employer_name' => 'Kapering Kidz',
+        'start_date' => '2014-09-01 00:00:00',
+        'end_date' => '2014-09-01 00:00:00',
+        'location' => 'Bridge of Don, Aberdeen',
+        'country' => 'UK',
+        'example' => false,
+      ]);
+    }
+}
