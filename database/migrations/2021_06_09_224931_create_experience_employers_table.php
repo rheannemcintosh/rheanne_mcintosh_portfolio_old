bb58f0417b81bc7f2aa6591232f5609d0b3e33bf
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2021-08-06 23:19:51 +0100

    Move the foreign key field for experiences

--- database/migrations/2021_06_09_224931_create_experience_employers_table.php
+++ database/migrations/2021_06_09_224931_create_experience_employers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateExperiencesTable extends Migration
+class CreateExperienceEmployersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,13 @@ class CreateExperiencesTable extends Migration
      */
     public function up()
     {
-        Schema::create('experiences', function (Blueprint $table) {
+        Schema::create('experience_employers', function (Blueprint $table) {
             $table->id();
-            $table->text('role');
-            $table->text('employment_type');
+            $table->text('employer_name');
             $table->date('start_date');
             $table->date('end_date');
+            $table->text('location');
+            $table->text('country');
             $table->boolean('example');
             $table->timestamps();
         });
@@ -31,6 +32,6 @@ class CreateExperiencesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('experiences');
+        Schema::dropIfExists('experiences_employers');
     }
 }
--- database/migrations/2021_06_10_224004_create_experiences_table.php
+++ database/migrations/2021_06_10_224004_create_experiences_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateExperienceEmployersTable extends Migration
+class CreateExperiencesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateExperienceEmployersTable extends Migration
      */
     public function up()
     {
-        Schema::create('experience_employers', function (Blueprint $table) {
+        Schema::create('experiences', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('experience_id')->constrained();
-            $table->text('employer_name');
+            $table->foreignId('experience_employer_id')->constrained();
+            $table->text('role');
+            $table->text('employment_type');
             $table->date('start_date');
             $table->date('end_date');
-            $table->text('location');
-            $table->text('country');
             $table->boolean('example');
             $table->timestamps();
         });
@@ -33,6 +32,6 @@ class CreateExperienceEmployersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('experiences_employers');
+        Schema::dropIfExists('experiences');
     }
 }
