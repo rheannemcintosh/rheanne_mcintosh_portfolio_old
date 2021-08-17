bf4a6b43c0bcf45a6465c16e31b3ef72f4eee6b6
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2021-08-17 23:07:05 +0100

    Create a new home page

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <div class="container mx-auto p-4">
+        <div class="p-4 lg:px-24">
+            <div class="bg-rmgrey shadow rounded-md p-8">
+            </div>
+        </div>
+    </div>
+</x-app-layout>
