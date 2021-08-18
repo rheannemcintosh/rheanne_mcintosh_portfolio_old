392663c16c2b480ef4086f3d84321c8d980b6b8b
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2021-08-18 23:41:40 +0100

    Update the homepage

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,7 +1,22 @@
 <x-app-layout>
     <div class="container mx-auto p-4">
         <div class="p-4 lg:px-24">
-            <div class="bg-rmgrey shadow rounded-md p-8">
+            <div class="bg-rmBlue shadow rounded-md p-8">
+                <h1 class="text-white text-10xl font-bebas">Hi, I'm Rheanne</h1>
+                <div class="py-10 my-4">
+                    <h2 class="italic text-white"><!-- Insert generic summary text --></h2>
+                </div>
+                <div class="grid mb-4 sm:grid-cols-3 grid-cols-1 gap-10">
+                    <div class="py-4 border-4 border-white hover:bg-rmBlue-light">
+                        <h3 class="text-3xl font-bebas text-center text-white">CV</h3>
+                    </div>
+                    <div class="py-4 border-4 border-white hover:bg-rmBlue-light">
+                        <h3 class="text-3xl font-bebas text-center text-white">Portfolio</h3>
+                    </div>
+                    <div class="py-4 border-4 border-white hover:bg-rmBlue-light">
+                        <h3 class="text-3xl font-bebas text-center text-white">Contact</h3>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
