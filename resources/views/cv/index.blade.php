18fd0548a742feb6901504f3a086c45447bd822c
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2021-09-08 21:49:39 +0100

    Temporarily remove the certificates section on site

--- resources/views/cv/index.blade.php
+++ resources/views/cv/index.blade.php
@@ -129,26 +129,6 @@
                             </div>
                         </div>
                         <!-- End of Personal Development Section -->
-
-                        <div class="py-12">
-                            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                                    <!-- This example requires Tailwind CSS v2.0+ -->
-                                    <div class="bg-white shadow overflow-hidden sm:rounded-lg">
-                                        <div class="px-4 py-5 sm:px-6">
-                                            <div class="px-4 py-5 sm:px-6">
-                                                <h3 class="text-lg leading-6 font-medium text-gray-900">
-                                                    Certificates
-                                                </h3>
-                                            </div>
-                                            <div class="border-t border-gray-200">
-                                                <p>----- Insert Certificates -----</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
