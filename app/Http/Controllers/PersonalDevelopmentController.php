cae0e0518599141b45db53076675b7b1f6bccdfc
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2021-09-15 22:42:38 +0100

    Create the Personal Development Controller

---
+++ app/Http/Controllers/PersonalDevelopmentController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PersonalDevelopmentController extends Controller
+{
+    public function index()
+    {
+        return view('errors.index');
+    }
+}
