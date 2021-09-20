13a16d4a98677abb4ca7f5c9403ceec8f4bd4e40
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2021-09-20 23:22:46 +0100

    Update the skill model with a CV and update the seeder with additional information

--- database/seeders/SkillSeeder.php
+++ database/seeders/SkillSeeder.php
@@ -17,123 +17,157 @@ class SkillSeeder extends Seeder
         \DB::table('skills')->delete();
 
         Skill::create([
-            'name' => 'PHP',
-            'skill_type_id' => 2,
-            'skill_proficiency_id' => 4,
-        ]);
-
-        Skill::create([
-            'name' => 'SQL',
-            'skill_type_id' => 2,
-            'skill_proficiency_id' => 4,
-        ]);
-
-        Skill::create([
-            'name' => 'MySQL',
-            'skill_type_id' => 6,
-            'skill_proficiency_id' => 4,
-        ]);
-
-        Skill::create([
-            'name' => 'Python',
-            'skill_type_id' => 2,
-            'skill_proficiency_id' => 3,
-        ]);
-
-        Skill::create([
-            'name' => 'JavaScript',
-            'skill_type_id' => 2,
-            'skill_proficiency_id' => 3,
-        ]);
-
-        Skill::create([
-            'name' => 'Java',
-            'skill_type_id' => 2,
-            'skill_proficiency_id' => 2,
-        ]);
-
-        Skill::create([
-            'name' => 'HTML',
-            'skill_type_id' => 2,
-            'skill_proficiency_id' => 4,
-        ]);
-
-        Skill::create([
-            'name' => 'CSS',
-            'skill_type_id' => 2,
-            'skill_proficiency_id' => 3,
-        ]);
-
-        Skill::create([
-            'name' => 'Laravel',
-            'skill_type_id' => 3,
-            'skill_proficiency_id' => 4,
-        ]);
-
-        Skill::create([
-            'name' => 'Git',
-            'skill_type_id' => 5,
-            'skill_proficiency_id' => 4,
-        ]);
-
-        Skill::create([
-            'name' => 'CodeIgniter',
-            'skill_type_id' => 3,
-            'skill_proficiency_id' => 3,
-        ]);
-
-        Skill::create([
-            'name' => 'Moodle',
-            'skill_type_id' => 8,
-            'skill_proficiency_id' => 2,
-        ]);
-
-        Skill::create([
-            'name' => 'React JS',
-            'skill_type_id' => 8,
-            'skill_proficiency_id' => 3,
-        ]);
-
-        Skill::create([
-            'name' => 'Flask',
-            'skill_type_id' => 3,
-            'skill_proficiency_id' => 2,
-        ]);
-
-        Skill::create([
-            'name' => 'Agile Methodologies',
-            'skill_type_id' => 5,
-            'skill_proficiency_id' => 4,
-        ]);
-
-        Skill::create([
-            'name' => 'WCAG',
-            'skill_type_id' => 5,
-            'skill_proficiency_id' => 3,
-        ]);
-
-        Skill::create([
-            'name' => 'Adobe Illustrator',
-            'skill_type_id' => 4,
-            'skill_proficiency_id' => 3,
-        ]);
-
-        Skill::create([
             'name' => 'English',
             'skill_type_id' => 1,
             'skill_proficiency_id' => 4,
+            'cv_flag' => true,
         ]);
 
         Skill::create([
             'name' => 'French',
             'skill_type_id' => 1,
             'skill_proficiency_id' => 2,
+            'cv_flag' => true,
         ]);
 
         Skill::create([
             'name' => 'German',
             'skill_type_id' => 1,
             'skill_proficiency_id' => 2,
+            'cv_flag' => true,
+        ]);
+
+        Skill::create([
+            'name' => 'PHP',
+            'skill_type_id' => 2,
+            'skill_proficiency_id' => 4,
+            'cv_flag' => true,
+        ]);
+
+        Skill::create([
+            'name' => 'SQL',
+            'skill_type_id' => 2,
+            'skill_proficiency_id' => 4,
+            'cv_flag' => true,
+        ]);
+
+        Skill::create([
+            'name' => 'Python',
+            'skill_type_id' => 2,
+            'skill_proficiency_id' => 3,
+            'cv_flag' => true,
+        ]);
+
+        Skill::create([
+            'name' => 'JavaScript',
+            'skill_type_id' => 2,
+            'skill_proficiency_id' => 3,
+            'cv_flag' => true,
+        ]);
+
+        Skill::create([
+            'name' => 'Java',
+            'skill_type_id' => 2,
+            'skill_proficiency_id' => 2,
+            'cv_flag' => true,
+        ]);
+
+        Skill::create([
+            'name' => 'HTML',
+            'skill_type_id' => 2,
+            'skill_proficiency_id' => 4,
+            'cv_flag' => true,
+        ]);
+
+        Skill::create([
+            'name' => 'CSS',
+            'skill_type_id' => 2,
+            'skill_proficiency_id' => 3,
+            'cv_flag' => true,
+        ]);
+
+        Skill::create([
+            'name' => 'Sass',
+            'skill_type_id' => 2,
+            'skill_proficiency_id' => 3,
+            'cv_flag' => true,
+        ]);
+
+        Skill::create([
+            'name' => 'Laravel',
+            'skill_type_id' => 3,
+            'skill_proficiency_id' => 4,
+            'cv_flag' => true,
+        ]);
+
+        Skill::create([
+            'name' => 'CodeIgniter',
+            'skill_type_id' => 3,
+            'skill_proficiency_id' => 3,
+            'cv_flag' => true,
+        ]);
+
+        Skill::create([
+            'name' => 'Flask',
+            'skill_type_id' => 3,
+            'skill_proficiency_id' => 2,
+            'cv_flag' => true,
+        ]);
+
+        Skill::create([
+            'name' => 'React JS',
+            'skill_type_id' => 3,
+            'skill_proficiency_id' => 3,
+            'cv_flag' => true,
+        ]);
+
+        Skill::create([
+            'name' => 'MySQL',
+            'skill_type_id' => 4,
+            'skill_proficiency_id' => 4,
+            'cv_flag' => true,
+        ]);
+
+        Skill::create([
+            'name' => 'Git',
+            'skill_type_id' => 4,
+            'skill_proficiency_id' => 4,
+            'cv_flag' => true,
+        ]);
+
+        Skill::create([
+            'name' => 'Moodle',
+            'skill_type_id' => 4,
+            'skill_proficiency_id' => 2,
+            'cv_flag' => true,
+        ]);
+
+        Skill::create([
+            'name' => 'Agile Methodologies',
+            'skill_type_id' => 5,
+            'skill_proficiency_id' => 4,
+            'cv_flag' => true,
+        ]);
+
+        Skill::create([
+            'name' => 'WCAG',
+            'skill_type_id' => 5,
+            'skill_proficiency_id' => 3,
+            'cv_flag' => true,
+        ]);
+
+        Skill::create([
+            'name' => 'Adobe Illustrator',
+            'skill_type_id' => 6,
+            'skill_proficiency_id' => 3,
+            'cv_flag' => true,
+        ]);
+
+        Skill::create([
+            'name' => 'Adobe InDesign',
+            'skill_type_id' => 6,
+            'skill_proficiency_id' => 3,
+            'cv_flag' => true,
         ]);
     }
 }
