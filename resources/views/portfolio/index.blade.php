ef440e5937bb64b886d67d03cd55480d01624d38
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2021-09-30 19:44:27 +0100

    Update the temporary portfolio page

--- resources/views/portfolio/index.blade.php
+++ resources/views/portfolio/index.blade.php
@@ -2,11 +2,11 @@
     <div class="container mx-auto p-4">
         <div class="p-4 lg:px-32 xl:px-56 2xl:px-96">
             <div class="p-8">
-                <h1 class="text-rmBlu-dark text-8xl sm:text-10xl font-bebas">Oops...</h1>
-                <h2 class="text-rmBlue-dark text-3xl font-bebas">This page is currently under construction, however please feel free to visit my <a href="https://github.com/rheannemcintosh">Github</a> in the meantime!</h2>
+                <h1 class="text-rmTeal text-8xl sm:text-10xl font-bebas">Oops...</h1>
+                <h2 class="text-rmBlue text-3xl font-bebas">This page is currently under construction, however please feel free to visit my <a href="https://github.com/rheannemcintosh">Github</a> in the meantime, to see some of my work!</h2>
                 <div class="pt-8 grid justify-items-center mx-auto">
-                    <a href="https://github.com/rheannemcintosh"><ion-icon class="text-8xl" name="logo-github"></ion-icon></a>
-                    <h3>@rheannemcintosh</h3>
+                    <a href="https://github.com/rheannemcintosh"><ion-icon class="text-8xl text-rmBlue" name="logo-github"></ion-icon></a>
+                    <h3 class="text-rmBlue-dark">@rheannemcintosh</h3>
                 </div>
             </div>
         </div>
