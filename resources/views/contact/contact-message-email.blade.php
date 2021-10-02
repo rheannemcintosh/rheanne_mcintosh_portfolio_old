38605ad1c09b079f5f2ffeb964d204bc7fc38ff4
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2021-10-02 17:31:04 +0100

    Create a basic email template

---
+++ resources/views/contact/contact-message-email.blade.php
@@ -0,0 +1,11 @@
+<h2>Hey !</h2> <br><br>
+
+You received an email from : {{ $first_name }} <br><br>
+
+User details: <br><br>
+
+Name:  {{ $first_name }} {{$last_name }}<br>
+Email:  {{ $email }}<br>
+Subject:  {{ $subject }}<br>
+Message: {{ $message_content }}<br><br>
+Thanks
