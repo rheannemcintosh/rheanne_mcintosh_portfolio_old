081e8ed61820cd43b7c9378382d0d2cf56611a12
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2021-10-03 22:58:06 +0100

    Update the contact form styling and add validation errors

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -1,90 +1,106 @@
 <x-app-layout>
     <div class="container mx-auto p-4">
         <div class="p-4 lg:px-24">
-            @if(Session::has('success'))
-                <div class="alert alert-success">
-                    {{ Session::get('success') }}
-                    @php
-                        Session::forget('success');
-                    @endphp
+            <div class="bg-rmWhite shadow rounded-md p-8">
+                <div class="mt-4">
+                    <form action="{{ url('/contact') }}" method="POST">
+                        {{ csrf_field() }}
+                        <div class="xl:px-20">
+                            <h1 class="pt-2 pl-2 text-rmBlue font-bebas text-9xl">Contact Me!</h1>
+                            <p class="mb-8"> Feel free to contact me below! Whether it be for freelance work,
+                                job opportunities, fellow nerds or if you just want a chat!</p>
+                            <div class="">
+                                <div id="name" class="form-group grid mb-4 sm:grid-cols-2 grid-cols-1 gap-4">
+                                    <div id="first_name">
+                                        <label for="first_name" class="block text-lg text-rmBlue font-bebas">First
+                                            Name</label>
+                                        <input type="text" name="first_name"
+                                               class="form-control rounded-md shadow-md w-full focus:ring-2 focus:ring-rmTeal-light focus:shadow-none focus:border-transparent"
+                                               placeholder="First Name" value="{{ old('first_name') }}">
+                                        @if ($errors->has('first_name'))
+                                            <span class="">{{ $errors->first('first_name') }}</span>
+                                        @endif
+                                    </div>
+                                    <div id="last_name">
+                                        <label for="last_name" class="block text-lg text-rmBlue font-bebas">Last
+                                            Name</label>
+                                        <input type="text" name="last_name"
+                                               class="form-control rounded-md shadow-md w-full focus:ring-2 focus:ring-rmTeal-light focus:shadow-none focus:border-transparent"
+                                               placeholder="Last Name" value="{{ old('last_name') }}">
+                                        @if ($errors->has('last_name'))
+                                            <span class="">{{ $errors->first('last_name') }}</span>
+                                        @endif
+                                    </div>
+                                </div>
+                                <div id="email" class="mb-4">
+                                    <label for="email"
+                                           class="block text-lg text-rmBlue font-bebas">Email</label>
+                                    <input type="text" name="email"
+                                           class="form-control rounded-md shadow-md w-full focus:ring-2 focus:ring-rmTeal-light focus:shadow-none focus:border-transparent"
+                                           placeholder="Email" value="{{ old('email') }}">
+                                    @if ($errors->has('email'))
+                                        <span class="">{{ $errors->first('email') }}</span>
+                                    @endif
+                                </div>
+                                <div id="subject" class="mb-4">
+                                    <label for="subject"
+                                           class="block text-lg text-rmBlue font-bebas">Subject</label>
+                                    <input type="text" name="subject"
+                                           class="form-control rounded-md shadow-md w-full focus:ring-2 focus:ring-rmTeal-light focus:shadow-none focus:border-transparent"
+                                           placeholder="Subject" value="{{ old('subject') }}">
+                                    @if ($errors->has('email'))
+                                        <span class="">{{ $errors->first('email') }}</span>
+                                    @endif
+                                </div>
+                                <div id="message" class="mb-4">
+                                    <label for="message_content"
+                                           class="mb-1  text-lg text-rmBlue block font-bebas">Message</label>
+                                    <textarea name="message_content" rows="3"
+                                              class="form-control rounded-md shadow-md w-full focus:ring-2 focus:ring-rmTeal-light focus:shadow-none focus:border-transparent"
+                                              placeholder="Message"
+                                              value="{{ old('message_content') }}"></textarea>
+                                    @if ($errors->has('message_content'))
+                                        <span class="">{{ $errors->first('message_content') }}</span>
+                                    @endif
+                                </div>
+                                <div id="reason" class="block mb-4">
+                                    <label class="mb-1 text-lg text-rmBlue block font-bebas">Reason For
+                                        Contacting <span class="italic">(Feel Free to Leave this Blank!)</span></label>
+                                    <div class="grid mb-4 lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-x-4 mt-2">
+                                        <div>
+                                            <label class="inline-flex items-center">
+                                                <input type="checkbox" class="form-checkbox">
+                                                <span class="ml-2">Freelance Enquiry</span>
+                                            </label>
+                                        </div>
+                                        <div>
+                                            <label class="inline-flex items-center">
+                                                <input type="checkbox" class="form-checkbox">
+                                                <span class="ml-2">Job Opportunity</span>
+                                            </label>
+                                        </div>
+                                        <div>
+                                            <label class="inline-flex items-center">
+                                                <input type="checkbox" class="form-checkbox">
+                                                <span class="ml-2">Fellow Nerds</span>
+                                            </label>
+                                        </div>
+                                        <div>
+                                            <label class="inline-flex items-center">
+                                                <input type="checkbox" class="form-checkbox">
+                                                <span class="ml-2">Casual Chat</span>
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <button class="bg-rmTeal-light hover:bg-rmTeal-dark text-white text-2xl font-bebas py-2 px-4 rounded">
+                                    Contact Me!
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-            @endif
-            <form action="{{ url('/contact') }}" method="POST">
-                {{ csrf_field() }}
-                <div class="bg-rmgrey shadow rounded-md p-8">
-                    <h1 class="text-5xl mb-4">Contact Me!</h1>
-
-                    <p class="mb-8"> Feel free to contact me below! Whether it be for freelance work, job opportunities, fellow nerds or if you just want a chat!</p>
-                    <div class="">
-                        <div id="name" class="grid mb-4 sm:grid-cols-2 grid-cols-1 gap-4">
-                            <div id="firstname">
-                                <label for="first_name" class="block">First Name</label>
-                                <input type="text" name="first_name" class="focus:shadow-none text-sm w-full rounded-md shadow-md">
-                            </div>
-                            <div id="lastname">
-                                <label for="last_name">Last Name</label>
-                                <input type="text" name="last_name" class="text-sm w-full rounded-md shadow-md">
-                            </div>
-                        </div>
-                        <div id="email" class="mb-4">
-                            <label for="email">Email</label>
-                            <input type="text" name="email" class="text-sm w-full rounded-md shadow-md">
-                        </div>
-                        <div id="subject" class="mb-4">
-                            <label for="subject">Subject</label>
-                            <input type="text" name="subject" class="text-sm w-full rounded-md shadow-md">
-                        </div>
-                        <div id="message" class="mb-4">
-                            <label class="mb-1" for="message_content">Message</label>
-                            <textarea name="message_content" rows="3" class="text-sm w-full text-sm rounded-md shadow-md"></textarea>
-                        </div>
-                        <div id="reason" class="block mb-4">
-                            <span class="text-gray-700">Checkboxes</span>
-                            <div class="mt-2">
-                                <div>
-                                    <label class="inline-flex items-center">
-                                        <input type="checkbox" class="form-checkbox">
-                                        <span class="ml-2">Freelance Enquiry</span>
-                                    </label>
-                                </div>
-                                <div>
-                                    <label class="inline-flex items-center">
-                                        <input type="checkbox" class="form-checkbox">
-                                        <span class="ml-2">Job Opportunity</span>
-                                    </label>
-                                </div>
-                                <div>
-                                    <label class="inline-flex items-center">
-                                        <input type="checkbox" class="form-checkbox">
-                                        <span class="ml-2">Fellow Nerds</span>
-                                    </label>
-                                </div>
-                                <div>
-                                    <label class="inline-flex items-center">
-                                        <input type="checkbox" class="form-checkbox">
-                                        <span class="ml-2">Casual Chat</span>
-                                    </label>
-                                </div>
-                            </div>
-                            <!-- Code to Update -->
-                            <!--
-                                <label class="mb-1" for="reason">Reason</label>
-                                <fieldset class="bg-gray-600 grid mb-4 sm:grid-cols-4 grid-cols-1 gap-4">
-                                    <input type="checkbox">
-                                    <label>Checkbox 1</label>
-                                    <input type="checkbox">
-                                    <label>Checkbox 2</label>
-                                    <input type="checkbox">
-                                    <label>Checkbox 3</label>
-                                </fieldset>
-                            -->
-                        </div>
-                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                            Contact Me!
-                        </button>
-                    </div>
-                </div>
-            </form>
+            </div>
         </div>
     </div>
 </x-app-layout>
