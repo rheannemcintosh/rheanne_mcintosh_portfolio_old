d2f720e201de89d6d0928e0f3e9febc03f95840a
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2021-10-06 22:05:27 +0100

    Update the home page to work as a tablet view

--- resources/views/home/test.blade.php
+++ resources/views/home/test.blade.php
@@ -35,20 +35,23 @@
                 <div class="container mx-auto p-4">
                     <div class="p-4 lg:px-24">
                         <div class="p-8">
-                            <h1 class="text-rmWhite text-center sm:text-10xl text-5xl font-bebas">Hi, I'm Rheanne!</h1>
-                            <div class="sm:py-8 py-4 sm:my-4 my-2"></div>
-                            <div class="grid mb-4 sm:grid-cols-4 grid-cols-1 gap-10">
+                            <h1 class="
+                                text-center
+                                text-rmWhite
+                                2xl:text-12xl xl:text-10xl lg:text-9xl md:text-8xl text-7xl font-bebas">Hi, I'm Rheanne!</h1>
+                            <div class="md:py-8 py-4 md:my-4 my-2"></div>
+                            <div class="grid mb-4 xl:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-10">
                                 <div class="py-4 border-4 border-rmWhite hover:bg-rmBlue-light">
-                                    <h3 class="sm:text-3xl text-xl font-bebas text-center text-white">About</h3>
+                                    <h3 class="md:text-3xl text-2xl font-bebas text-center text-white">About</h3>
                                 </div>
                                 <div class="py-4 border-4 border-rmWhite hover:bg-rmBlue-light">
-                                    <h3 class="sm:text-3xl text-xl font-bebas text-center text-rmWhite">Portfolio</h3>
+                                    <h3 class="md:text-3xl text-2xl font-bebas text-center text-rmWhite">Portfolio</h3>
                                 </div>
                                 <div class="py-4 border-4 border-rmWhite hover:bg-rmBlue-light">
-                                    <h3 class="sm:text-3xl text-xl font-bebas text-center text-rmWhite">Contact</h3>
+                                    <h3 class="md:text-3xl text-2xl font-bebas text-center text-rmWhite">Contact</h3>
                                 </div>
                                 <div class="py-4 border-4 border-rmWhite hover:bg-rmBlue-light">
-                                    <h3 class="sm:text-3xl text-xl font-bebas text-center text-rmWhite">Blog</h3>
+                                    <h3 class="md:text-3xl text-2xl font-bebas text-center text-rmWhite">Blog</h3>
                                 </div>
                             </div>
                         </div>
