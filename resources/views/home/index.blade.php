b30ebdd54cb8f01ebaae94ae0372f9125697a4fc
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2021-10-25 23:33:12 +0100

    Update the home page view

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,23 +1,56 @@
-<x-app-layout>
-    <div class="container mx-auto p-4">
-        <div class="p-4 lg:px-24">
-            <div class="bg-rmBlue shadow rounded-md p-8">
-                <h1 class="text-white text-10xl font-bebas">Hi, I'm Rheanne</h1>
-                <div class="py-10 my-4">
-                    <h2 class="italic text-white"><!-- Insert generic summary text --></h2>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+    <!-- Head -->
+    @include('includes.head')
+
+    <!-- Body -->
+    <body
+            class="bg-rmMuted bg-cover font-sans antialiased"
+            x-data="{navOpen: false, scrolledFromTop: false}"
+            x-init="window.pageYOffset >= 50 ? scrolledFromTop = true : scrolledFromTop = false"
+            @scroll.window="window.pageYOffset >= 50 ? scrolledFromTop = true : scrolledFromTop = false"
+            :class="{
+            'overflow-hidden': navOpen,
+            'overflow-scroll': !navOpen
+        }"
+    >
+
+    @include('includes.nav-home')
+
+        <!-- Page Content -->
+        <div class="page-wrap min-h-screen pt-20">
+            <main>
+                <div class="container mx-auto p-4">
+                    <div class="p-4 lg:px-24">
+                        <div class="p-8">
+                            <h1 class="
+                                text-center
+                                text-rmWhite
+                                2xl:text-12xl xl:text-10xl lg:text-9xl md:text-8xl text-7xl font-bebas">Hi, I'm Rheanne!</h1>
+                            <div class="md:py-8 py-4 md:my-4 my-2"></div>
+                            <div class="grid mb-4 xl:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-10">
+                                <div class="py-4 border-4 border-rmWhite hover:bg-rmBlue-light">
+                                    <h3 class="md:text-3xl text-2xl font-bebas text-center text-white">About</h3>
+                                </div>
+                                <div class="py-4 border-4 border-rmWhite hover:bg-rmBlue-light">
+                                    <h3 class="md:text-3xl text-2xl font-bebas text-center text-rmWhite">Portfolio</h3>
+                                </div>
+                                <div class="py-4 border-4 border-rmWhite hover:bg-rmBlue-light">
+                                    <h3 class="md:text-3xl text-2xl font-bebas text-center text-rmWhite">Contact</h3>
+                                </div>
+                                <div class="py-4 border-4 border-rmWhite hover:bg-rmBlue-light">
+                                    <h3 class="md:text-3xl text-2xl font-bebas text-center text-rmWhite">Blog</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="grid mb-4 sm:grid-cols-3 grid-cols-1 gap-10">
-                    <div class="py-4 border-4 border-white hover:bg-rmBlue-light">
-                        <h3 class="text-3xl font-bebas text-center text-white">CV</h3>
-                    </div>
-                    <div class="py-4 border-4 border-white hover:bg-rmBlue-light">
-                        <h3 class="text-3xl font-bebas text-center text-white">Portfolio</h3>
-                    </div>
-                    <div class="py-4 border-4 border-white hover:bg-rmBlue-light">
-                        <h3 class="text-3xl font-bebas text-center text-white">Contact</h3>
-                    </div>
-                </div>
-            </div>
+            </main>
         </div>
-    </div>
-</x-app-layout>
+
+        <!-- Footer -->
+        @include('includes.footer')
+
+    </body>
+</html>
