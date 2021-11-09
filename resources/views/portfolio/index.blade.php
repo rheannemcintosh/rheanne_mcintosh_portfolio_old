4bbfe727b9f62c7e9bb77e3cb011a3dcc3cb9ccb
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2021-11-09 23:04:03 +0000

    Update margins and padding

--- resources/views/portfolio/index.blade.php
+++ resources/views/portfolio/index.blade.php
@@ -6,11 +6,11 @@
                     @foreach ($portfolio_projects as $project)
                         <a href="{{$project->github_url}}">
                             <div class="bg-rmWhite rounded-lg shadow p-4 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
-                                <div class="bg-{{$project->colour->colour}} p-4 2xl:text-6xl lg:text-5xl text-5xl h-60 mb-2">
+                                <div class="bg-{{$project->colour->colour}} p-4 2xl:text-6xl lg:text-5xl text-5xl h-60 mb-4">
                                     <h1 class="text-white font-bebas">{{$project->name}}</h1>
                                 </div>
                                 <div class="flex">
-                                    <div class="flex-none inline-block bg-{{$project->skill->colour->colour}} text-white font-bebas py-0.5 px-4 rounded-full">
+                                    <div class="flex-none inline-block bg-{{$project->skill->colour->colour}} text-white font-bebas pb-0.5 pt-1 px-4 rounded-full">
                                         <h1>{{$project->skill->name}}</h1>
                                     </div>
                                     <div class="ml-2 flex-none inline-block bg-rmGrey text-rmBlue-dark font-bebas pb-0.5 pt-1 px-4 rounded-full">
